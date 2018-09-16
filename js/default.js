minbrowser/min

commit 13c0fc0148e3c8ee3d341288bb7a72136bf66661

Changed code style to Standard

js/default.js
@@ -1,25 +1,25 @@
-window.electron = require("electron");
-window.ipc = electron.ipcRenderer;
-window.remote = electron.remote;
-window.Dexie = require("dexie");
+window.electron = require('electron')
+window.ipc = electron.ipcRenderer
+window.remote = electron.remote
+window.Dexie = require('dexie')
 
-//disable dragdrop, since it currently doesn't work
-window.addEventListener("drop", function (e) {
-	e.preventDefault();
-});
+// disable dragdrop, since it currently doesn't work
+window.addEventListener('drop', function (e) {
+  e.preventDefault()
+})
 
-//add a class to the body for fullscreen status
+// add a class to the body for fullscreen status
 
-ipc.on("enter-full-screen", function () {
-	document.body.classList.add("fullscreen");
-});
+ipc.on('enter-full-screen', function () {
+  document.body.classList.add('fullscreen')
+})
 
-ipc.on("leave-full-screen", function () {
-	document.body.classList.remove("fullscreen");
-});
+ipc.on('leave-full-screen', function () {
+  document.body.classList.remove('fullscreen')
+})
 
-window.addEventListener("load", function (e) {
-	if (navigator.platform != "MacIntel") {
-		document.body.classList.add("notMac");
-	}
-});
+window.addEventListener('load', function (e) {
+  if (navigator.platform !== 'MacIntel') {
+    document.body.classList.add('notMac')
+  }
+})

js/webviews.js
@@ -1,281 +1,273 @@
 /* implements selecting webviews, switching between them, and creating new ones. */
 
-var phishingWarningPage = "file://" + __dirname + "/pages/phishing/index.html"; //TODO move this somewhere that actually makes sense
-var crashedWebviewPage = "file:///" + __dirname + "/pages/crash/index.html";
-var errorPage = "file:///" + __dirname + "/pages/error/index.html"
+var phishingWarningPage = 'file://' + __dirname + '/pages/phishing/index.html' // TODO move this somewhere that actually makes sense
+var crashedWebviewPage = 'file:///' + __dirname + '/pages/crash/index.html'
+var errorPage = 'file:///' + __dirname + '/pages/error/index.html'
 
-var webviewBase = document.getElementById("webviews");
-var webviewEvents = [];
-var webviewIPC = [];
+var webviewBase = document.getElementById('webviews')
+var webviewEvents = []
+var webviewIPC = []
 
-//this only affects newly created webviews, so all bindings should be done on startup
+// this only affects newly created webviews, so all bindings should be done on startup
 
-function bindWebviewEvent(event, fn) {
-	webviewEvents.push({
-		event: event,
-		fn: fn,
-	})
+function bindWebviewEvent (event, fn) {
+  webviewEvents.push({
+    event: event,
+    fn: fn
+  })
 }
 
-//function is called with (webview, tabId, IPCArguements)
+// function is called with (webview, tabId, IPCArguements)
 
-function bindWebviewIPC(name, fn) {
-	webviewIPC.push({
-		name: name,
-		fn: fn,
-	})
+function bindWebviewIPC (name, fn) {
+  webviewIPC.push({
+    name: name,
+    fn: fn
+  })
 }
 
-//the permissionRequestHandler used for webviews
-function pagePermissionRequestHandler(webContents, permission, callback) {
-	if (permission === "notifications" || permission === "fullscreen") {
-		callback(true);
-	} else {
-		callback(false);
-	}
+// the permissionRequestHandler used for webviews
+function pagePermissionRequestHandler (webContents, permission, callback) {
+  if (permission === 'notifications' || permission === 'fullscreen') {
+    callback(true)
+  } else {
+    callback(false)
+  }
 }
 
-//called whenever the page url changes
+// called whenever the page url changes
 
-function onPageLoad(e) {
-	var tab = this.getAttribute("data-tab");
-	var url = this.getAttribute("src"); //src attribute changes whenever a page is loaded
+function onPageLoad (e) {
+  var tab = this.getAttribute('data-tab')
+  var url = this.getAttribute('src') // src attribute changes whenever a page is loaded
 
-	if (url.indexOf("https://") === 0 || url.indexOf("about:") == 0 || url.indexOf("chrome:") == 0 || url.indexOf("file://") == 0) {
-		tabs.update(tab, {
-			secure: true,
-			url: url,
-		});
-	} else {
-		tabs.update(tab, {
-			secure: false,
-			url: url,
-		});
-	}
+  if (url.indexOf('https://') === 0 || url.indexOf('about:') === 0 || url.indexOf('chrome:') === 0 || url.indexOf('file://') === 0) {
+    tabs.update(tab, {
+      secure: true,
+      url: url
+    })
+  } else {
+    tabs.update(tab, {
+      secure: false,
+      url: url
+    })
+  }
 
-	var isInternalPage = url.indexOf(__dirname) != -1 && url.indexOf(readerView.readerURL) == -1
+  var isInternalPage = url.indexOf(__dirname) !== -1 && url.indexOf(readerView.readerURL) === -1
 
-	if (tabs.get(tab).private == false && !isInternalPage) { //don't save to history if in private mode, or the page is a browser page
-		bookmarks.updateHistory(tab);
-	}
+  if (tabs.get(tab).private === false && !isInternalPage) { // don't save to history if in private mode, or the page is a browser page
+    bookmarks.updateHistory(tab)
+  }
 
-	rerenderTabElement(tab);
+  rerenderTabElement(tab)
 
-	this.send("loadfinish"); //works around an electron bug (https://github.com/atom/electron/issues/1117), forcing Chromium to always  create the script context
-
+  this.send('loadfinish') // works around an electron bug (https://github.com/atom/electron/issues/1117), forcing Chromium to always  create the script context
 }
 
-//set the permissionRequestHandler for non-private tabs
+// set the permissionRequestHandler for non-private tabs
 
-remote.session.defaultSession.setPermissionRequestHandler(pagePermissionRequestHandler);
+remote.session.defaultSession.setPermissionRequestHandler(pagePermissionRequestHandler)
 
-function getWebviewDom(options) {
+function getWebviewDom (options) {
+  var w = document.createElement('webview')
+  w.setAttribute('preload', 'dist/webview.min.js')
 
-	var w = document.createElement("webview");
-	w.setAttribute("preload", "dist/webview.min.js");
+  if (options.url) {
+    w.setAttribute('src', urlParser.parse(options.url))
+  }
 
-	if (options.url) {
-		w.setAttribute("src", urlParser.parse(options.url));
-	}
+  w.setAttribute('data-tab', options.tabId)
 
-	w.setAttribute("data-tab", options.tabId);
+  // if the tab is private, we want to partition it. See http://electron.atom.io/docs/v0.34.0/api/web-view-tag/#partition
+  // since tab IDs are unique, we can use them as partition names
+  if (tabs.get(options.tabId).private === true) {
+    var partition = options.tabId.toString() // options.tabId is a number, which remote.session.fromPartition won't accept. It must be converted to a string first
 
-	//if the tab is private, we want to partition it. See http://electron.atom.io/docs/v0.34.0/api/web-view-tag/#partition
-	//since tab IDs are unique, we can use them as partition names
-	if (tabs.get(options.tabId).private == true) {
-		var partition = options.tabId.toString(); //options.tabId is a number, which remote.session.fromPartition won't accept. It must be converted to a string first
+    w.setAttribute('partition', partition)
 
-		w.setAttribute("partition", partition);
+    // register permissionRequestHandler for this tab
+    // private tabs use a different session, so the default permissionRequestHandler won't apply
 
-		//register permissionRequestHandler for this tab
-		//private tabs use a different session, so the default permissionRequestHandler won't apply
+    remote.session.fromPartition(partition).setPermissionRequestHandler(pagePermissionRequestHandler)
 
-		remote.session.fromPartition(partition).setPermissionRequestHandler(pagePermissionRequestHandler);
+    // enable ad/tracker/contentType blocking in this tab if needed
 
-		//enable ad/tracker/contentType blocking in this tab if needed
+    registerFiltering(partition)
+  }
 
-		registerFiltering(partition);
-	}
+  // webview events
 
-	//webview events
+  webviewEvents.forEach(function (i) {
+    w.addEventListener(i.event, i.fn)
+  })
 
-	webviewEvents.forEach(function (i) {
-		w.addEventListener(i.event, i.fn);
-	});
+  w.addEventListener('page-favicon-updated', function (e) {
+    var id = this.getAttribute('data-tab')
+    updateTabColor(e.favicons, id)
+  })
 
-	w.addEventListener("page-favicon-updated", function (e) {
-		var id = this.getAttribute("data-tab");
-		updateTabColor(e.favicons, id);
-	});
+  w.addEventListener('page-title-set', function (e) {
+    var tab = this.getAttribute('data-tab')
+    tabs.update(tab, {
+      title: e.title
+    })
+    rerenderTabElement(tab)
+  })
 
-	w.addEventListener("page-title-set", function (e) {
-		var tab = this.getAttribute("data-tab");
-		tabs.update(tab, {
-			title: e.title
-		});
-		rerenderTabElement(tab);
-	});
+  w.addEventListener('did-finish-load', onPageLoad)
+  w.addEventListener('did-navigate-in-page', onPageLoad)
 
-	w.addEventListener("did-finish-load", onPageLoad);
-	w.addEventListener("did-navigate-in-page", onPageLoad);
+  /* w.on("did-get-redirect-request", function (e) {
+  	console.log(e.originalEvent)
+  }); */
 
-	/*w.on("did-get-redirect-request", function (e) {
-		console.log(e.originalEvent);
-	});*/
+  // open links in new tabs
 
-	//open links in new tabs
+  w.addEventListener('new-window', function (e) {
+    var tab = this.getAttribute('data-tab')
+    var currentIndex = tabs.getIndex(tabs.getSelected())
 
-	w.addEventListener("new-window", function (e) {
-		var tab = this.getAttribute("data-tab");
-		var currentIndex = tabs.getIndex(tabs.getSelected());
+    var newTab = tabs.add({
+      url: e.url,
+      private: tabs.get(tab).private // inherit private status from the current tab
+    }, currentIndex + 1)
+    addTab(newTab, {
+      enterEditMode: false,
+      openInBackground: e.disposition === 'background-tab' // possibly open in background based on disposition
+    })
+  })
 
-		var newTab = tabs.add({
-			url: e.url,
-			private: tabs.get(tab).private //inherit private status from the current tab
-		}, currentIndex + 1);
-		addTab(newTab, {
-			enterEditMode: false,
-			openInBackground: e.disposition == "background-tab", //possibly open in background based on disposition
-		});
-	});
+  w.addEventListener('close', function (e) {
+    var tabId = this.getAttribute('data-tab')
+    var selTab = tabs.getSelected()
+    var currentIndex = tabs.getIndex(tabId)
+    var nextTab = tabs.getAtIndex(currentIndex - 1) || tabs.getAtIndex(currentIndex + 1)
 
-	w.addEventListener("close", function (e) {
-		var tabId = this.getAttribute("data-tab");
-		var selTab = tabs.getSelected();
-		var currentIndex = tabs.getIndex(tabId);
-		var nextTab = tabs.getAtIndex(currentIndex - 1) || tabs.getAtIndex(currentIndex + 1);
+    destroyTab(tabId)
 
-		destroyTab(tabId);
+    if (tabId === selTab) { // the tab being destroyed is the current tab, find another tab to switch to
+      if (nextTab) {
+        switchToTab(nextTab.id)
+      } else {
+        addTab()
+      }
+    }
+  })
 
-		if (tabId == selTab) { //the tab being destroyed is the current tab, find another tab to switch to
+  // In embedder page. Send the text content to bookmarks when recieved.
+  w.addEventListener('ipc-message', function (e) {
+    var w = this
+    var tab = this.getAttribute('data-tab')
 
-			if (nextTab) {
-				switchToTab(nextTab.id);
-			} else {
-				addTab();
-			}
-		}
-	})
+    webviewIPC.forEach(function (item) {
+      if (item.name === e.channel) {
+        item.fn(w, tab, e.args)
+      }
+    })
 
+    if (e.channel === 'bookmarksData') {
+      bookmarks.onDataRecieved(e.args[0])
+    } else if (e.channel === 'phishingDetected') {
+      // check if the page is on the phishing detection whitelist
 
-	// In embedder page. Send the text content to bookmarks when recieved.
-	w.addEventListener('ipc-message', function (e) {
-		var w = this;
-		var tab = this.getAttribute("data-tab");
+      var url = w.getAttribute('src')
 
-		webviewIPC.forEach(function (item) {
-			if (item.name == e.channel) {
-				item.fn(w, tab, e.args);
-			}
-		});
+      try {
+        var hostname = new URL(url).hostname
+      } catch (e) {
+        var hostname = ''
+      }
 
-		if (e.channel == "bookmarksData") {
-			bookmarks.onDataRecieved(e.args[0]);
+      settings.get('phishingWhitelist', function (value) {
+        if (!value || !hostname || value.indexOf(hostname) === -1) {
+          // show the warning page
+          navigate(tab, phishingWarningPage + '?url=' + encodeURIComponent(url))
+        }
+      }, {
+        fromCache: false
+      })
+    }
+  })
 
-		} else if (e.channel == "phishingDetected") {
-			//check if the page is on the phishing detection whitelist
+  w.addEventListener('contextmenu', webviewMenu.show)
 
-			var url = w.getAttribute("src");
+  w.addEventListener('crashed', function (e) {
+    var tabId = this.getAttribute('data-tab')
 
-			try {
-				var hostname = new URL(url).hostname;
-			} catch (e) {
-				var hostname = "";
-			}
+    destroyWebview(tabId)
+    tabs.update(tabId, {
+      url: crashedWebviewPage
+    })
 
-			settings.get("phishingWhitelist", function (value) {
-				if (!value || !hostname || value.indexOf(hostname) == -1) {
-					//show the warning page
-					navigate(tab, phishingWarningPage + "?url=" + encodeURIComponent(url));
-				}
-			}, {
-				fromCache: false
-			});
-		}
-	});
+    addWebview(tabId)
+    switchToWebview(tabId)
+  })
 
-	w.addEventListener("contextmenu", webviewMenu.show);
+  w.addEventListener('did-fail-load', function (e) {
+    if (e.errorCode !== -3 && e.validatedURL === e.target.getURL()) {
+      navigate(this.getAttribute('data-tab'), errorPage + '?ec=' + encodeURIComponent(e.errorCode) + '&url=' + e.target.getURL())
+    }
+  })
 
-	w.addEventListener("crashed", function (e) {
-		var tabId = this.getAttribute("data-tab");
+  w.addEventListener('enter-html-full-screen', function (e) {
+    this.classList.add('fullscreen')
+  })
 
-		destroyWebview(tabId);
-		tabs.update(tabId, {
-			url: crashedWebviewPage
-		});
+  w.addEventListener('leave-html-full-screen', function (e) {
+    this.classList.remove('fullscreen')
+  })
 
-		addWebview(tabId);
-		switchToWebview(tabId);
-	});
-
-	w.addEventListener("did-fail-load", function (e) {
-		if (e.errorCode != -3 && e.validatedURL == e.target.getURL()) {
-			navigate(this.getAttribute("data-tab"), errorPage + "?ec=" + encodeURIComponent(e.errorCode) + "&url=" + e.target.getURL());
-		}
-	});
-
-	w.addEventListener("enter-html-full-screen", function (e) {
-		this.classList.add("fullscreen");
-	});
-
-	w.addEventListener("leave-html-full-screen", function (e) {
-		this.classList.remove("fullscreen");
-	})
-
-	return w;
-
+  return w
 }
 
-/* options: openInBackground: should the webview be opened without switching to it? default is false. 
- */
+/* options: openInBackground: should the webview be opened without switching to it? default is false. */
 
-var WebviewsWithHiddenClass = false;
+var WebviewsWithHiddenClass = false
 
-function addWebview(tabId) {
+function addWebview (tabId) {
+  var tabData = tabs.get(tabId)
 
-	var tabData = tabs.get(tabId);
+  var webview = getWebviewDom({
+    tabId: tabId,
+    url: tabData.url
+  })
 
-	var webview = getWebviewDom({
-		tabId: tabId,
-		url: tabData.url
-	});
+  // this is used to hide the webview while still letting it load in the background
+  // webviews are hidden when added - call switchToWebview to show it
+  webview.classList.add('hidden')
 
-	//this is used to hide the webview while still letting it load in the background
-	//webviews are hidden when added - call switchToWebview to show it
-	webview.classList.add("hidden");
+  webviewBase.appendChild(webview)
 
-	webviewBase.appendChild(webview);
-
-	return webview;
+  return webview
 }
 
-function switchToWebview(id) {
-	var webviews = document.getElementsByTagName("webview");
-	for (var i = 0; i < webviews.length; i++) {
-		webviews[i].hidden = true;
-	}
+function switchToWebview (id) {
+  var webviews = document.getElementsByTagName('webview')
+  for (var i = 0; i < webviews.length; i++) {
+    webviews[i].hidden = true
+  }
 
-	var wv = getWebview(id);
+  var wv = getWebview(id)
 
-	if (!wv) {
-		wv = addWebview(id);
-	}
+  if (!wv) {
+    wv = addWebview(id)
+  }
 
-	wv.classList.remove("hidden");
-	wv.hidden = false;
+  wv.classList.remove('hidden')
+  wv.hidden = false
 }
 
-function updateWebview(id, url) {
-	getWebview(id).setAttribute("src", urlParser.parse(url));
+function updateWebview (id, url) {
+  getWebview(id).setAttribute('src', urlParser.parse(url))
 }
 
-function destroyWebview(id) {
-	var w = document.querySelector('webview[data-tab="{id}"]'.replace("{id}", id));
-	if (w) {
-		w.parentNode.removeChild(w);
-	}
+function destroyWebview (id) {
+  var w = document.querySelector('webview[data-tab="{id}"]'.replace('{id}', id))
+  if (w) {
+    w.parentNode.removeChild(w)
+  }
 }
 
-function getWebview(id) {
-	return document.querySelector('webview[data-tab="{id}"]'.replace("{id}", id));
+function getWebview (id) {
+  return document.querySelector('webview[data-tab="{id}"]'.replace('{id}', id))
 }

js/webview/default.js
@@ -1,5 +1,5 @@
 /* imports common modules */
 
-var electron = require("electron");
-var ipc = electron.ipcRenderer;
-var webFrame;
+var electron = require('electron')
+var ipc = electron.ipcRenderer
+var webFrame

js/api-wrapper.js
@@ -1,126 +1,120 @@
 /* common to webview, tabrenderer, etc */
 
-function navigate(tabId, newURL) {
-	newURL = urlParser.parse(newURL);
+function navigate (tabId, newURL) {
+  newURL = urlParser.parse(newURL)
 
-	tabs.update(tabId, {
-		url: newURL
-	});
+  tabs.update(tabId, {
+    url: newURL
+  })
 
-	updateWebview(tabId, newURL);
+  updateWebview(tabId, newURL)
 
-	leaveTabEditMode({
-		blur: true
-	});
+  leaveTabEditMode({
+    blur: true
+  })
 }
 
-function destroyTask(id) {
-	var task = tasks.get(id);
+function destroyTask (id) {
+  var task = tasks.get(id)
 
-	task.tabs.forEach(function (tab) {
-		destroyWebview(tab.id);
-	});
+  task.tabs.forEach(function (tab) {
+    destroyWebview(tab.id)
+  })
 
-	tasks.destroy(id);
+  tasks.destroy(id)
 }
 
-//destroys the webview and tab element for a tab
-function destroyTab(id) {
+// destroys the webview and tab element for a tab
+function destroyTab (id) {
+  var tabEl = getTabElement(id)
+  tabEl.parentNode.removeChild(tabEl)
 
-	var tabEl = getTabElement(id);
-	tabEl.parentNode.removeChild(tabEl);
-
-	var t = tabs.destroy(id); //remove from state - returns the index of the destroyed tab
-	destroyWebview(id); //remove the webview
-
+  var t = tabs.destroy(id) // remove from state - returns the index of the destroyed tab
+  destroyWebview(id) // remove the webview
 }
 
-//destroys a tab, and either switches to the next tab or creates a new one
-function closeTab(tabId) {
+// destroys a tab, and either switches to the next tab or creates a new one
+function closeTab (tabId) {
+  /* disabled in focus mode */
+  if (isFocusMode) {
+    showFocusModeError()
+    return
+  }
 
-	/* disabled in focus mode */
-	if (isFocusMode) {
-		showFocusModeError();
-		return;
-	}
+  if (tabId === tabs.getSelected()) {
+    var currentIndex = tabs.getIndex(tabs.getSelected())
+    var nextTab = tabs.getAtIndex(currentIndex - 1) || tabs.getAtIndex(currentIndex + 1)
 
-	if (tabId == tabs.getSelected()) {
-		var currentIndex = tabs.getIndex(tabs.getSelected());
-		var nextTab = tabs.getAtIndex(currentIndex - 1) || tabs.getAtIndex(currentIndex + 1);
+    destroyTab(tabId)
 
-		destroyTab(tabId);
-
-		if (nextTab) {
-			switchToTab(nextTab.id);
-		} else {
-			addTab();
-		}
-
-	} else {
-		destroyTab(tabId);
-	}
+    if (nextTab) {
+      switchToTab(nextTab.id)
+    } else {
+      addTab()
+    }
+  } else {
+    destroyTab(tabId)
+  }
 }
 
-function switchToTask(id) {
-	tasks.setSelected(id);
+function switchToTask (id) {
+  tasks.setSelected(id)
 
-	rerenderTabstrip();
+  rerenderTabstrip()
 
-	var taskData = tasks.get(id);
+  var taskData = tasks.get(id)
 
-	if (taskData.tabs.length > 0) {
-		var selectedTab = taskData.tabs.getSelected();
-		
-		//if the task has no tab that is selected, switch to the most recent one
-		
-		if(!selectedTab) {
-			selectedTab = taskData.tabs.get().sort(function(a, b) {
-				return b.lastActivity - a.lastActivity;
-			})[0].id;
-		}
-		
-		switchToTab(selectedTab);
-	} else {
-		addTab();
-	}
+  if (taskData.tabs.length > 0) {
+    var selectedTab = taskData.tabs.getSelected()
+
+    // if the task has no tab that is selected, switch to the most recent one
+
+    if (!selectedTab) {
+      selectedTab = taskData.tabs.get().sort(function (a, b) {
+        return b.lastActivity - a.lastActivity
+      })[0].id
+    }
+
+    switchToTab(selectedTab)
+  } else {
+    addTab()
+  }
 }
 
 /* switches to a tab - update the webview, state, tabstrip, etc. */
 
-function switchToTab(id, options) {
+function switchToTab (id, options) {
+  options = options || {}
 
-	options = options || {};
+  /* tab switching disabled in focus mode */
+  if (isFocusMode) {
+    showFocusModeError()
+    return
+  }
 
-	/* tab switching disabled in focus mode */
-	if (isFocusMode) {
-		showFocusModeError();
-		return;
-	}
+  leaveTabEditMode()
 
-	leaveTabEditMode();
+  tabs.setSelected(id)
+  setActiveTabElement(id)
+  switchToWebview(id)
 
-	tabs.setSelected(id);
-	setActiveTabElement(id);
-	switchToWebview(id);
+  if (options.focusWebview !== false) {
+    getWebview(id).focus()
+  }
 
-	if (options.focusWebview != false) {
-		getWebview(id).focus();
-	}
+  var tabData = tabs.get(id)
+  setColor(tabData.backgroundColor, tabData.foregroundColor)
 
-	var tabData = tabs.get(id);
-	setColor(tabData.backgroundColor, tabData.foregroundColor);
+  // we only want to mark the tab as active if someone actually interacts with it. If it is clicked on and then quickly clicked away from, it should still be marked as inactive
 
-	//we only want to mark the tab as active if someone actually interacts with it. If it is clicked on and then quickly clicked away from, it should still be marked as inactive
+  setTimeout(function () {
+    if (tabs.get(id) && tabs.getSelected() === id) {
+      tabs.update(id, {
+        lastActivity: Date.now()
+      })
+      tabActivity.refresh()
+    }
+  }, 2500)
 
-	setTimeout(function () {
-		if (tabs.get(id) && tabs.getSelected() == id) {
-			tabs.update(id, {
-				lastActivity: Date.now(),
-			});
-			tabActivity.refresh();
-		}
-	}, 2500);
-
-	sessionRestore.save();
-
+  sessionRestore.save()
 }

Capybara003/browser

commit 00b2c5662795364f4132dec29118513defc92e71

fix resizing bugs
throttle was broken by 13c0fc0148e3c8ee3d341288bb7a72136bf66661

Also fixed issue when resizing window while placeholder is visible

js/default.js
@@ -41,7 +41,7 @@ function throttle (fn, threshhold, scope) {
   return function () {
     var context = scope || this
 
-    var now = new Date()
+    var now = +new Date
     var args = arguments
     if (last && now < last + threshhold) {
       // hold on to it

js/webviews.js
@@ -343,8 +343,12 @@ webviews.bindEvent('new-window', function (e, url, frameName, disposition) {
 }, {preventDefault: true})
 
 window.addEventListener('resize', throttle(function () {
+  if (webviews.placeholderRequests.length > 0) {
+    // can't set view bounds if the view is hidden
+    return
+  }
   ipc.send('setBounds', {id: webviews.selectedId, bounds: getViewBounds()})
-}, 100))
+}, 75))
 
 ipc.on('enter-html-full-screen', function () {
   windowIsFullscreen = true