Capybara003/browser

commit 7ce7a9b38f0eed51107c58c978691ac810739088

Convert the taskPrototype to a TaskList class (#622)
* Convert the array-like `tasks` object to a TaskList instance

Makes it easier to reason about, because now it is not a mix of an array
and an object.
Instead, the array is kept as a reference inside TaskList,
and the global tasks variable is an instance of it.

Avoids the need of iterating a prototype object and assigning its props
to an array.

* Get rid of the parameter-less TaskList::get(). Provide helpers instead

In some places tasks.get() is used to access a task by its index, e.g.:
tasks.get()[length - 1]. Instead, TaskList should offer a neat way to
accomplish the same but for now, use tasks.tasks[index].

* Implement TaskList::indexOf to simplify search by task

* Simplify the determination of the next/previous task using the modulo op

* Add TaskList::byIndex(), not to access TaskLists internals from outside

* Define getTaskByNameOrNumber in terms of TaskList::find() for simplicity

Removes completely usage of tasks.tasks, i.e. TaskList's internals are
now not accessed from anywhere.

* Simplify TaskList methods by using the provided find() helpers

Converted bare loops to a more functional style for clarity

* Simplify TaskList::update by not throwing away the already found task

Keep a reference to the task found and operate directly on it. No need
to search for it twice.

* Pass a task reference to TaskList::update instead of id

Removes the need for null checks in the update method

* No need to sort tasks to find the latest, reduce() instead

* Remove TaskList::update() as it's not relevant to the TaskList context

Also, it was used only once, with only a single param to update. It's
expressed simpler as done in the commit.

* Fix style of retrieving the nextTask

https://github.com/minbrowser/min/pull/622#discussion_r219024295

* Implement TaskList::find() with a for loop instead of the built in func

js/tabState/task.js
@@ -1,17 +1,18 @@
-const tabPrototype = require("tabState/tab.js")
-const TabStack = require("tabRestore.js")
+const tabPrototype = require('tabState/tab.js')
+const TabStack = require('tabRestore.js')
 
-const taskPrototype = {
-  add: function (task, index) {
-    if (!task) {
-      task = {}
-    }
+class TaskList {
+  constructor () {
+    this.selected = null
+    this.tasks = [] // each task is {id, name, tabs: [], tabHistory: TabStack}
+  }
 
-    var newTask = {
+  add (task = {}, index) {
+    const newTask = {
       name: task.name || null,
       tabs: task.tabs || [],
       tabHistory: new TabStack(task.tabHistory),
-      id: task.id || String(getRandomId())
+      id: task.id || String(TaskList.getRandomId())
     }
 
     for (var key in tabPrototype) {
@@ -19,75 +20,60 @@ const taskPrototype = {
     }
 
     if (index) {
-      tabState.tasks.splice(index, 0, newTask)
+      this.tasks.splice(index, 0, newTask)
     } else {
-      tabState.tasks.push(newTask)
+      this.tasks.push(newTask)
     }
 
     return newTask.id
-  },
-  get: function (id) {
-    if (!id) {
-      return tabState.tasks
-    }
+  }
 
-    for (var i = 0; i < tabState.tasks.length; i++) {
-      if (tabState.tasks[i].id === id) {
-        return tabState.tasks[i]
-      }
+  getStringifyableState () {
+    return {
+      tasks: this.tasks,
+      selectedTask: this.selected
     }
-    return null
-  },
-  getTaskContainingTab: function (tabId) {
-    for (var i = 0; i < tabState.tasks.length; i++) {
-      if (tabState.tasks[i].tabs.has(tabId)) {
-        return tabState.tasks[i]
-      }
-    }
-    return null
-  },
-  getIndex: function (id) {
-    for (var i = 0; i < tabState.tasks.length; i++) {
-      if (tabState.tasks[i].id === id) {
-        return i
-      }
-    }
-    return -1
-  },
-  setSelected: function (id) {
-    tabState.selectedTask = id
-    window.currentTask = tasks.get(id)
+  }
+
+  get (id) {
+    return this.find(task => task.id == id) || null
+  }
+
+  byIndex (index) {
+    return this.tasks[index]
+  }
+
+  getTaskContainingTab (tabId) {
+    return this.find(task => task.tabs.has(tabId)) || null
+  }
+
+  getIndex (id) {
+    return this.tasks.findIndex(task => task.id == id)
+  }
+
+  setSelected (id) {
+    this.selected = id
+    window.currentTask = this.get(id)
     window.tabs = currentTask.tabs
-  },
-  destroy: function (id) {
-    for (var i = 0; i < tabState.tasks.length; i++) {
-      if (tabState.tasks[i].id === id) {
-        tabState.tasks.splice(i, 1)
-        return i
-      }
-    }
-    return false
-  },
-  destroyAll: function () {
-    tabState.tasks = []
-    currentTask = null
-  },
-  update: function (id, data) {
-    if (!tasks.get(id)) {
-      throw new ReferenceError('Attempted to update a task that does not exist.')
-    }
+  }
 
-    for (var i = 0; i < tabState.tasks.length; i++) {
-      if (tabState.tasks[i].id === id) {
-        for (var key in data) {
-          tabState.tasks[i][key] = data[key]
-        }
-        break
-      }
-    }
-  },
-  getLastActivity: function (id) {
-    var tabs = tasks.get(id).tabs
+  destroy (id) {
+    const index = this.getIndex(id)
+
+    if(index < 0) return false
+
+    this.tasks.splice(index, 1)
+    return index
+  }
+
+  destroyAll () {
+    this.tasks = []
+    this.selected = null
+    currentTask = null
+  }
+
+  getLastActivity (id) {
+    var tabs = this.get(id).tabs
     var lastActivity = 0
 
     for (var i = 0; i < tabs.length; i++) {
@@ -98,10 +84,28 @@ const taskPrototype = {
 
     return lastActivity
   }
+
+  getLength() {
+    return this.tasks.length
+  }
+
+  map(fun) { return this.tasks.map(fun) }
+
+  forEach(fun) { return this.tasks.forEach(fun) }
+
+  indexOf(task) { return this.tasks.indexOf(task) }
+
+  find(filter) {
+    for (var i = 0, len = this.tasks.length; i < len; i++) {
+      if (filter(this.tasks[i])) {
+        return this.tasks[i]
+      }
+    } 
+  }
+
+  static getRandomId () {
+    return Math.round(Math.random() * 100000000000000000)
+  }
 }
 
-function getRandomId () {
-  return Math.round(Math.random() * 100000000000000000)
-}
-
-module.exports = taskPrototype
+module.exports = TaskList

js/taskOverlay/taskOverlay.js
@@ -69,7 +69,7 @@ window.taskOverlay = {
     empty(taskContainer)
 
     // show the task elements
-    tasks.get().forEach(function (task, index) {
+    tasks.forEach(function (task, index) {
       const el = createTaskContainer(task, index)
 
       taskContainer.appendChild(el)

js/api-wrapper.js
@@ -38,7 +38,6 @@ options
   options.openInBackground - whether to open the tab without switching to it. Defaults to false.
 */
 function addTab (tabId = tabs.add(), options = {}) {
-
   tabBar.addTab(tabId)
   webviews.add(tabId)
 
@@ -79,22 +78,22 @@ function closeTask (taskId) {
   if (taskId === currentTask.id) {
     // the current task was destroyed, find another task to switch to
 
-    if (tasks.get().length === 0) {
+    if (tasks.getLength() === 0) {
       // there are no tasks left, create a new one
       return addTask()
     } else {
       // switch to the most-recent task
 
-      var recentTaskList = tasks.get().map(function (task) {
+      var recentTaskList = tasks.map(function (task) {
         return { id: task.id, lastActivity: tasks.getLastActivity(task.id) }
       })
 
-      // sort the tasks based on how recent they are
-      recentTaskList.sort(function (a, b) {
-        return b.lastActivity - a.lastActivity
-      })
+      const mostRecent = recentTaskList.reduce(
+        (latest, current) =>
+          current.lastActivity > latest.lastActivity ? current : latest
+      )
 
-      return switchToTask(recentTaskList[0].id)
+      return switchToTask(mostRecent.id)
     }
   }
 }
@@ -109,7 +108,8 @@ function closeTab (tabId) {
 
   if (tabId === tabs.getSelected()) {
     var currentIndex = tabs.getIndex(tabs.getSelected())
-    var nextTab = tabs.getAtIndex(currentIndex - 1) || tabs.getAtIndex(currentIndex + 1)
+    var nextTab =
+      tabs.getAtIndex(currentIndex - 1) || tabs.getAtIndex(currentIndex + 1)
 
     destroyTab(tabId)
 
@@ -183,4 +183,14 @@ function switchToTab (id, options) {
   tabActivity.refresh()
 }
 
-module.exports = {navigate, addTask, addTab, destroyTask, destroyTab, closeTask, closeTab, switchToTask, switchToTab}
+module.exports = {
+  navigate,
+  addTask,
+  addTab,
+  destroyTask,
+  destroyTab,
+  closeTask,
+  closeTab,
+  switchToTask,
+  switchToTab
+}

js/keybindings.js
@@ -353,15 +353,10 @@ settings.get('keyMap', function (keyMapSettings) {
   defineShortcut('switchToNextTask', function (d) {
     taskOverlay.show()
 
-    var currentTaskIdx = tasks.get().map(function (task) {
-      return task.id
-    }).indexOf(currentTask.id)
+    const currentTaskIdx = tasks.indexOf(currentTask)
 
-    if (tasks.get()[currentTaskIdx + 1]) {
-      browserUI.switchToTask(tasks.get()[currentTaskIdx + 1].id)
-    } else {
-      browserUI.switchToTask(tasks.get()[0].id)
-    }
+    const nextTask = tasks.byIndex(currentTaskIdx + 1) || tasks.byIndex(0)
+    browserUI.switchToTask(nextTask.id)
 
     taskOverlay.show()
 
@@ -374,15 +369,11 @@ settings.get('keyMap', function (keyMapSettings) {
   defineShortcut('switchToPreviousTask', function (d) {
     taskOverlay.show()
 
-    var currentTaskIdx = tasks.get().map(function (task) {
-      return task.id
-    }).indexOf(currentTask.id)
+    const currentTaskIdx = tasks.indexOf(currentTask),
+          taskCount = tasks.getLength()
 
-    if (tasks.get()[currentTaskIdx - 1]) {
-      browserUI.switchToTask(tasks.get()[currentTaskIdx - 1].id)
-    } else {
-      browserUI.switchToTask(tasks.get()[tasks.get().length - 1].id)
-    }
+    const previousTask = tasks.byIndex(currentTaskIdx - 1) || tasks.byIndex(tasks.getLength() - 1)
+    browserUI.switchToTask(previousTask.id)
 
     taskOverlay.show()
 

js/searchbar/customBangs.js
@@ -74,16 +74,11 @@ registerCustomBang({
 
 // returns a task with the same name or index ("1" returns the first task, etc.)
 function getTaskByNameOrNumber (text) {
-  var taskSet = tasks.get()
+  const textAsNumber = parseInt(text)
 
-  var textAsNumber = parseInt(text)
-
-  for (var i = 0; i < taskSet.length; i++) {
-    if ((taskSet[i].name && taskSet[i].name.toLowerCase() === text) || i + 1 === textAsNumber) {
-      return taskSet[i]
-    }
-  }
-  return null
+  return tasks.find((task, index) =>
+    (task.name && task.name.toLowerCase() === text) || index + 1 === textAsNumber
+  )
 }
 
 registerCustomBang({
@@ -91,7 +86,6 @@ registerCustomBang({
   snippet: l('switchToTask'),
   isAction: false,
   fn: function (text) {
-
     /* disabled in focus mode */
     if (focusMode.enabled()) {
       focusMode.warn()
@@ -119,7 +113,6 @@ registerCustomBang({
   snippet: l('createTask'),
   isAction: true,
   fn: function (text) {
-
     /* disabled in focus mode */
     if (focusMode.enabled()) {
       focusMode.warn()
@@ -142,7 +135,6 @@ registerCustomBang({
   snippet: l('moveToTask'),
   isAction: false,
   fn: function (text) {
-
     /* disabled in focus mode */
     if (focusMode.enabled()) {
       focusMode.warn()

js/searchbar/openTabsPlugin.js
@@ -12,7 +12,7 @@ var searchOpenTabs = function (text, input, event, container) {
   var searchText = text.toLowerCase()
   var currentTab = currentTask.tabs.getSelected()
 
-  tasks.get().forEach(function (task) {
+  tasks.forEach(function (task) {
     task.tabs.get().forEach(function (tab) {
       if (tab.id === currentTab || !tab.title || tab.url === 'about:blank') {
         return

js/sessionRestore.js
@@ -4,7 +4,7 @@ window.sessionRestore = {
   save: function () {
     var data = {
       version: 2,
-      state: JSON.parse(JSON.stringify(tabState))
+      state: JSON.parse(JSON.stringify(tasks.getStringifyableState()))
     }
 
     // save all tabs that aren't private

js/tabState.js
@@ -1,15 +1,7 @@
-var taskPrototype = require("tabState/task.js")
+const TaskList  = require("tabState/task.js")
 
 function initializeTabState () {
-  window.tabState = {
-    tasks: [], // each task is {id, name, tabs: [], tabHistory: TabStack}
-    selectedTask: null
-  }
-  for (var key in taskPrototype) {
-    tabState.tasks[key] = taskPrototype[key]
-  }
-
-  window.tasks = tabState.tasks
+  window.tasks = new TaskList()
   window.currentTask = undefined
   window.tabs = undefined
 }

commit 07ff12003cca0438086e3871a3c316583938c0c3

fix rearranging tasks
regression from #622

js/tabState/task.js
@@ -7,7 +7,7 @@ class TaskList {
     this.tasks = [] // each task is {id, name, tabs: [], tabHistory: TabStack}
   }
 
-  add (task = {}, index) {
+  add (task = {} , index) {
     const newTask = {
       name: task.name || null,
       tabs: task.tabs || [],
@@ -60,7 +60,7 @@ class TaskList {
   destroy (id) {
     const index = this.getIndex(id)
 
-    if(index < 0) return false
+    if (index < 0) return false
 
     this.tasks.splice(index, 1)
     return index
@@ -85,22 +85,24 @@ class TaskList {
     return lastActivity
   }
 
-  getLength() {
+  getLength () {
     return this.tasks.length
   }
 
-  map(fun) { return this.tasks.map(fun) }
+  map (fun) { return this.tasks.map(fun) }
 
-  forEach(fun) { return this.tasks.forEach(fun) }
+  forEach (fun) { return this.tasks.forEach(fun) }
 
-  indexOf(task) { return this.tasks.indexOf(task) }
+  indexOf (task) { return this.tasks.indexOf(task) }
 
-  find(filter) {
+  splice (...args) { return this.tasks.splice.apply(this.tasks, args) }
+
+  find (filter) {
     for (var i = 0, len = this.tasks.length; i < len; i++) {
       if (filter(this.tasks[i])) {
         return this.tasks[i]
       }
-    } 
+    }
   }
 
   static getRandomId () {

js/taskOverlay/taskOverlay.js
@@ -1,7 +1,7 @@
 var browserUI = require('api-wrapper.js')
 var focusMode = require('focusMode.js')
 
-const createTaskContainer = require("taskOverlay/taskOverlayBuilder.js")
+const createTaskContainer = require('taskOverlay/taskOverlayBuilder.js')
 
 var taskContainer = document.getElementById('task-area')
 var taskSwitcherButton = document.getElementById('switch-task-button')
@@ -195,7 +195,7 @@ taskOverlay.taskDragula.on('drop', function (el, target, source, sibling) {
   if (adjacentTaskId) {
     var newIdx = tasks.getIndex(adjacentTaskId)
   } else {
-    var newIdx = tasks.length
+    var newIdx = tasks.getLength()
   }
 
   // reinsert the task